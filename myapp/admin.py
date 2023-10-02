fa013dfcbcf44a3f4bce4c3dcf5eb764c9c8640f
Álvaro Martinez Ferreira <alvaromartinezferreira@gmail.com>
2023-10-02 17:00:12 -0400

    change

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import MyModel
+from .models import Atividade,Evento,Palestrante,Participante
 
-admin.site.register(MyModel)
+admin.site.register(Atividade)
+admin.site.register(Evento)
+admin.site.register(Participante)
+admin.site.register(Palestrante)
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,37 @@
 from django.db import models
 
-class MyModel(models.Model):
-    content = models.CharField(max_length=200)
+class Evento(models.Model):
+    nome = models.CharField(max_length=200)
     date_pub = models.DateTimeField("My datetime")
+    organizador = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
 
-    def __str__(self):
-        return self.content,self.date_pub
+class Palestrante(models.Model):
+    user_id = models.ForeignKey(
+        "Users",
+        on_delete=models.CASCADE,
+    )
+class Atividade(models.Model):
+    descricao = models.TextField()
+    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200)
+    data_pub = models.DateTimeField("My datetime")
+    inicio = models.DateTimeField("My datetime")
+    fim = models.DateTimeField("My datetime")
+    palestrante = models.ForeignKey(
+        'Palestrante',
+        on_delete=models.CASCADE,
+    )
+
+class Participante(models.Model):
+    user = models.ForeignKey(
+        'Users',
+        on_delete=models.CASCADE,
+    )
+    Atividade = models.ForeignKey(
+        'Atividade',
+        on_delete=models.CASCADE,
+    )
+    data_reg = models.DateTimeField("My datetime")
